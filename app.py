c9c2e65b71626b9ddf328276bcf561ac4d080953
faruqputra <umarulfaruqputra@gmail.com>
2026-01-16 11:32:40 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -2,29 +2,30 @@ import streamlit as st
 import joblib
 import numpy as np
 
+# Load model dan scaler
 model = joblib.load("model_dt.pkl")
 scaler = joblib.load("scaler.pkl")
 
 st.title("Prediksi Risiko Osteoporosis")
 
-# ========== INPUT SESUAI DATASET ==========
-usia = st.selectbox("Usia (tahun)", list(range(18, 101)))
+# ========== INPUT (TIDAK BISA DIKETIK) ==========
+usia = st.slider("Usia (tahun)", 18, 100, 30)
 
 jenis_kelamin = st.selectbox("Jenis Kelamin", ["Laki-laki", "Perempuan"])
 perubahan_hormon = st.selectbox("Perubahan Hormon", ["Normal", "Pasca menopause"])
-riwayat_keluarga = st.selectbox("Riwayat Keluarga Osteoporosis", ["Tidak", "Ya"])
+riwayat_keluarga = st.selectbox("Riwayat Keluarga Osteoporosis", ["Tidak", "Iya"])
 ras = st.selectbox("Ras / Etnis", ["Asia", "Kaukasia", "Afrika-Amerika"])
 berat_badan = st.selectbox("Berat Badan", ["Normal", "Kurus"])
 kalsium = st.selectbox("Asupan Kalsium", ["Cukup", "Rendah"])
 vitamin_d = st.selectbox("Asupan Vitamin D", ["Cukup", "Tidak cukup"])
 aktivitas = st.selectbox("Aktivitas Fisik", ["Aktif", "Kurang aktif"])
 merokok = st.selectbox("Merokok", ["Tidak", "Ya"])
-alkohol = st.selectbox("Konsumsi Alkohol", ["Tidak ada data", "Sedang"])
+alkohol = st.selectbox("Konsumsi Alkohol", ["Tidak", "Iya"])
 kondisi_medis = st.selectbox("Kondisi Medis", ["Gangguan tiroid", "Tidak ada", "Radang sendi"])
 obat = st.selectbox("Obat-obatan", ["Kortikosteroid", "Tidak ada"])
 patah = st.selectbox("Riwayat Patah Tulang", ["Tidak", "Ya"])
 
-# ========== MAPPING SESUAI MODEL ==========
+# ========== MAPPING KE ANGKA ==========
 map_jk = {"Perempuan":0,"Laki-laki":1}
 map_hormon = {"Normal":0,"Pasca menopause":1}
 map_keluarga = {"Tidak":0,"Ya":1}
@@ -39,7 +40,7 @@ map_medis = {"Gangguan tiroid":0,"Tidak ada":1,"Radang sendi":2}
 map_obat = {"Kortikosteroid":0,"Tidak ada":1}
 map_patah = {"Tidak":0,"Ya":1}
 
-# ========== DATA KE ARRAY ==========
+# ========== SUSUN DATA ==========
 data = np.array([[
     usia,
     map_jk[jenis_kelamin],
@@ -63,6 +64,6 @@ data_scaled = scaler.transform(data)
 if st.button("Prediksi"):
     hasil = model.predict(data_scaled)
     if hasil[0] == 1:
-        st.error("⚠️ Pasien berisiko mengalami Osteoporosis")
+        st.error("⚠️ Pasien Berisiko Mengalami Osteoporosis")
     else:
-        st.success("✅ Pasien tidak berisiko Osteoporosis")
+        st.success("✅ Pasien Tidak Berisiko Osteoporosis")
